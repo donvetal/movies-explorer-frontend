6e222c8b270ccbb71bd9876f1dcc515d54115603
Vitaliy <vitaliymontana@yandex.ru>
2021-10-29 00:58:42 +0300

    add preloader

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Movies explorer frontend
+      </h1>
+    </div>
+  );
+}
+
+export default App;
